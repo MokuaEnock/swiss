4c31474c3476d893cabfeb43a4d4803e1581d8c7
Enock <enock.mokua@student.moringaschool.com>
2023-01-10 01:27:00 +0300

    add heading to patiets feeling

--- src/pages/Patient.js
+++ src/pages/Patient.js
@@ -25,14 +25,30 @@ export default function Patient() {
             <span id="patient-bio-"></span>
             <span id="patient-bio-"></span>
           </div>
-          
+
           <div id="patient-details">
             <span>Nairobi, Kenya</span>
             <span>Male</span>
             <span>Outpatient</span>
             <span>Malaria</span>
           </div>
-          <div id="patient-bio"></div>
+
+          <div id="patient-feeling">
+            <h4>Patient's feeling</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              vitae faucibus tortor. Nulla facilisi. Sed tristique fringilla
+              lorem et vulputate. Suspendisse varius finibus ligula sit amet
+              fermentum. Ut pellentesque semper posuere. Aenean a augue et ante
+              tincidunt faucibus. Sed lorem elit, vestibulum et erat eget,
+              tempor accumsan tortor. Suspendisse vitae enim id ante porta
+              fermentum. Nullam dolor lorem, vulputate sed porttitor id,
+              malesuada nec risus. Sed quis vestibulum arcu, at condimentum
+              elit. Mauris et purus suscipit, fermentum dui at, aliquam elit.
+              Aliquam ante tellus, vehicula in felis et, porta finibus lorem.
+              Aliquam tempus sapien sed neque convallis ultrices at a.
+            </p>
+          </div>
           <div id="patient-bio"></div>
           <div id="patient-bio"></div>
 
