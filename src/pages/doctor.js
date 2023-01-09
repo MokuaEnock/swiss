a7ed336b2a14322f7bd7305058e780e7ee655901
Enock <enock.mokua@student.moringaschool.com>
2023-01-09 03:25:58 +0300

    add doctor links

--- src/pages/doctor.js
+++ src/pages/doctor.js
@@ -1,7 +1,14 @@
 export default function Doctor() {
   return (
     <main id="doctor">
-      <header id="doctor-header">header</header>
+      <header id="doctor-header">
+        <span id="doctor-logo">Swiss Medicare</span>
+        <span className="doctor-links">Home</span>
+        <span className="doctor-links">Patients</span>
+        <span className="doctor-links">Profile</span>
+        {/* <span className="doctor-links"></span> */}
+        <span id="doctor-logout">Log Out</span>
+      </header>
       <section id="doctor-section">section</section>
       <aside id="doctor-aside">aside</aside>
     </main>
