6981f73ff2b48d94ff8832457abd0ce69a57625d
Enock <enock.mokua@student.moringaschool.com>
2023-01-09 07:47:16 +0300

    connect patient to main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Doctor from "./pages/doctor";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Patient from "./pages/Patient";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/doctor" element={<Doctor />} />
+        <Route path="/doctor" element={<Patient />} />
       </Routes>
       {/* <Doctor /> */}
     </BrowserRouter>
