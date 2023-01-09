dfa64af49999bdc19d0b56c07fb84c9ee4afbbc0
Enock <enock.mokua@student.moringaschool.com>
2023-01-09 03:58:22 +0300

    add doctor stats structure and default styling

--- src/pages/doctor.js
+++ src/pages/doctor.js
@@ -9,7 +9,17 @@ export default function Doctor() {
         {/* <span className="doctor-links"></span> */}
         <span id="doctor-logout">Log Out</span>
       </header>
-      <section id="doctor-section">section</section>
+
+      <section id="doctor-section">
+        <div id="doctor-stats">
+          <span className="doctor-stat"></span>
+          <span className="doctor-stat"></span>
+          <span className="doctor-stat"></span>
+          <span className="doctor-stat"></span>
+        </div>
+        <div id="doctor-container"></div>
+      </section>
+
       <aside id="doctor-aside">aside</aside>
     </main>
   );
