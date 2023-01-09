366e9f4e7670614ddae33b6b72c838ffd99004ba
Enock <enock.mokua@student.moringaschool.com>
2023-01-09 07:41:56 +0300

    add components to patient page

--- src/pages/Patient.js
+++ src/pages/Patient.js
@@ -1,4 +1,18 @@
 import "./patient.css";
 export default function Patient() {
-  return <section id="patient-section"></section>;
+  return (
+    <main id="doctor">
+      <header id="doctor-header">
+        <span id="doctor-logo">Swiss Medicare</span>
+        <span className="doctor-links">Home</span>
+        <span className="doctor-links">Patients</span>
+        <span className="doctor-links">Profile</span>
+        <span id="doctor-logout">Log Out</span>
+      </header>
+
+      <section id="doctor-section"></section>
+
+      <aside id="doctor-aside"></aside>
+    </main>
+  );
 }
