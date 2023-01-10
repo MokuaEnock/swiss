c4766bdfd0ca4ce7c717f95b172643521a199989
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:36:20 +0300

    add doctor card

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -19,13 +19,13 @@ export default function Services() {
       </section>
 
       <section className="services">
-        <h3 className="services-header">Services</h3>
+        <h3 className="services-header">Meet Our Doctors</h3>
         <div className="services-container">
-          <span className="services-card"></span>
-          <span className="services-card"></span>
-          <span className="services-card"></span>
-          <span className="services-card"></span>
+          <span className="doctor-card"></span>
+          <span className="doctor-card"></span>
+          <span className="doctor-card"></span>
         </div>
+
         <div className="services-footer">
           <span className="services-footer-span"></span>
           <span className="services-footer-span"></span>
