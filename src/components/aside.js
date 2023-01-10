a012d1f68b5e0606d61cc8fbc01d896dad4f04db
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 02:24:15 +0300

    format footer and add background color

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,14 +1,18 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-header"></div>
-      <div className="footer-container">
-        <span className="footer-span"></span>
-        <span className="footer-span"></span>
-        <span className="footer-span"></span>
-        <span className="footer-span"></span>
+      <div>
+        <div className="footer-header"></div>
+        <div className="footer-container">
+          <span className="footer-span"></span>
+          <span className="footer-span"></span>
+          <span className="footer-span"></span>
+          <span className="footer-span"></span>
+        </div>
+        <div className="footer-footer">
+          <p>Swis Medicare 2023 | Proudly powered by Swis Medicare</p>
+        </div>
       </div>
-      <div className="footer-footer"></div>
     </footer>
   );
 }
