e966663a4750c15eb2e506bfae84d67d23c9d9e7
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 00:23:52 +0300

    create main section componenta and connect it to home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import MainSection from "./mainsection";
 
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="main">
+      <MainSection />
+    </main>
+  );
 }
