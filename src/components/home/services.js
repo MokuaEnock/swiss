e61ef0bccade672f2f025ebe9656630522786707
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:13:14 +0300

    create default structure for services section

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -1,5 +1,13 @@
 export default function Services() {
-  return <section>
-    
-  </section>;
+  return (
+    <section className="services">
+      <div className="services-header"></div>
+      <div>
+        <span className="services-card"></span>
+        <span className="services-card"></span>
+        <span className="services-card"></span>
+      </div>
+      <div className="services-footer"></div>
+    </section>
+  );
 }
