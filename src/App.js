027dcea71a416c356016f892835d4351ece022a2
Enock <enock.mokua@student.moringaschool.com>
2023-01-10 03:47:23 +0300

    add nested routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Doctor from "./pages/doctor";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Patient from "./pages/Patient";
+import Aside from "./components/aside";
+import Header from "./components/header";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/doctor" element={<Doctor />} />
-        <Route path="/patient" element={<Patient />} />
+        <Route path="/doctor" element={<Doctor />}>
+          <Header />
+          <Route path="/doctor/:userId" element={Patient} />
+          <Aside />
+        </Route>
       </Routes>
       {/* <Doctor /> */}
     </BrowserRouter>
