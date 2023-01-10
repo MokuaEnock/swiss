416f69c5ad75c4caa2fda3f0b41bb72413b9b84d
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 00:15:34 +0300

    edit header logo

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,15 @@ export default function Header() {
   return (
     <header id="header">
       <div id="header-logo">
-        <Link to="/">Logo</Link>
+        <Link to="/">Swiss Medicare</Link>
       </div>
 
       <div id="header-container">
         <Link to="/patient">Patient</Link>
         <Link to="/doctor">Doctor</Link>
-        {/* <Link to="/patient">Patient</Link> */}
+        <Link to="/patient">About Us</Link>
+        <Link to="/patient">Testimonials</Link>
+        <Link to="/patient">Contact Us</Link>
       </div>
 
       <div id="header-auth">
