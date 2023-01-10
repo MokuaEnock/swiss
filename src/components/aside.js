d4601403e892bf346279b7afa45925f6a3d3396c
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 02:42:29 +0300

    frmat structure for footer and add content

--- src/components/aside.js
+++ src/components/aside.js
@@ -7,7 +7,14 @@ export default function Footer() {
         <div className="footer-header"></div>
         <div className="footer-container">
           <span className="footer-span"></span>
-          <span className="footer-span"></span>
+          <span className="footer-span">
+            <h5>About</h5>
+            <Link>Doctors</Link>
+            <Link>Hospital</Link>
+            <Link>Contact</Link>
+            <Link>Address</Link>
+            <p></p>
+          </span>
           <span className="footer-span">
             <h5>Terms</h5>
             <Link>Privacy</Link>
@@ -15,7 +22,14 @@ export default function Footer() {
             <Link>Site Map</Link>
             <p></p>
           </span>
-          <span className="footer-span"></span>
+          <span className="footer-span">
+            <h5>About</h5>
+            <p>
+              Swis Medicare is an online health service center, that has unique
+              features and high services.
+            </p>
+            <p></p>
+          </span>
         </div>
         <div className="footer-footer">
           <p>Swis Medicare 2023 | Proudly powered by Swis Medicare</p>
