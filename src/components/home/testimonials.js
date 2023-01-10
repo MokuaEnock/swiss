f887cea0478bed3b2134b373440e7a4592d6bddf
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:59:55 +0300

    style testimonials structure

--- src/components/home/testimonials.js
+++ src/components/home/testimonials.js
@@ -1,7 +1,7 @@
 export default function Testimonials() {
   return (
     <section className="testimonials">
-      <div className="test-header"></div>
+      <h3 className="services-header">Testimonials</h3>
       <div className="test-conts">
         <span className="test-cards"></span>
         <span className="test-cards"></span>
