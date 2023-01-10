c3f33736ef450598b0da8af20baf27a62961a24f
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:52:14 +0300

    add structure for testimonials section

--- src/components/home/testimonials.js
+++ src/components/home/testimonials.js
@@ -1,3 +1,11 @@
 export default function Testimonials() {
-  return <section className="testimonials"></section>;
+  return (
+    <section className="testimonials">
+      <div className="test-header"></div>
+      <div className="test-conts">
+        <span className="test-cards"></span>
+        <span className="test-cards"></span>
+      </div>
+    </section>
+  );
 }
