f5f4e43d76b4f19fe03abe98bffda7800a8b5071
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:27:06 +0300

    add basic styling to services

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -1,13 +1,19 @@
+import "./home.css";
+
 export default function Services() {
   return (
     <section className="services">
       <h3 className="services-header">Services</h3>
-      <div>
+      <div className="services-container">
         <span className="services-card"></span>
         <span className="services-card"></span>
         <span className="services-card"></span>
       </div>
-      <div className="services-footer"></div>
+      <div className="services-footer">
+        <span className="services-footer-span"></span>
+        <span className="services-footer-span"></span>
+        <span className="services-footer-span"></span>
+      </div>
     </section>
   );
 }
