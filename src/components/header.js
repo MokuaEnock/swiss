30249d8582a35161ac835bf15ad248c7b45c07aa
Enock <enock.mokua@student.moringaschool.com>
2023-01-10 23:44:35 +0300

    create header links

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <header id="header"></header>;
+  return (
+    <header id="header">
+      <div id="header-logo">
+        <span></span>
+      </div>
+
+      <div id="header-container">
+        <Link to="/patient">Patient</Link>
+        <Link to="/doctor">Patient</Link>
+        <Link to="/patient">Patient</Link>
+      </div>
+
+      <div id="header-auth">
+        <Link to="/signup">Patient</Link>
+      </div>
+    </header>
+  );
 }
