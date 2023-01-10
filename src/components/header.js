1fd04eec4bab197b8a423e626dab16685290abf9
Enock <enock.mokua@student.moringaschool.com>
2023-01-10 03:38:37 +0300

    create header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header id="doctor-header">
+      <span id="doctor-logo">Swiss Medicare</span>
+      <span className="doctor-links">Home</span>
+      <span className="doctor-links">Patients</span>
+      <span className="doctor-links">Profile</span>
+      <span id="doctor-logout">Log Out</span>
+    </header>
+  );
+}
