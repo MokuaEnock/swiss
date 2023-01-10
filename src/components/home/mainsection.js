90b3414528d9b9249d4f84d69a31be9813e93b26
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 00:36:47 +0300

    create structure of the main section

--- src/components/home/mainsection.js
+++ src/components/home/mainsection.js
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom";
 import "./home.css";
 
 export default function MainSection() {
   return (
     <section className="main-section">
-      <div className="main-section-cont"></div>
-      <div className="main-section-cont"></div>
+      <div className="main-section-cont">
+        <h2>
+          Welcome to <br />
+          <strong>Swiss Medicare</strong>
+        </h2>
+
+        <p>Your health is our concern.</p>
+
+        <Link to="/signup" className="main-section-cont-button">
+          Join Us
+        </Link>
+
+        <span>
+          <img src="#" alt="home" />
+        </span>
+      </div>
+      <div className="main-section-cont" id="main-section-cont-image">
+        <img src="#" alt="doctor" />
+      </div>
     </section>
   );
 }
