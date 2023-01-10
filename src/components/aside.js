abde1b4393e2e70840f7aedc7e1616a09405c174
Enock <enock.mokua@student.moringaschool.com>
2023-01-10 23:40:18 +0300

    create Header for the react page

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,3 +1,3 @@
-export default function Aside() {
-  return <aside id="doctor-aside"></aside>;
+export default function Footer() {
+  return <footer id="doctor-aside"></footer>;
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,3 @@
 export default function Header() {
-  return (
-    <header id="doctor-header">
-      <span id="doctor-logo">Swiss Medicare</span>
-      <span className="doctor-links">Home</span>
-      <span className="doctor-links">Patients</span>
-      <span className="doctor-links">Profile</span>
-      <span id="doctor-logout">Log Out</span>
-    </header>
-  );
+  return <header id="header"></header>;
 }
