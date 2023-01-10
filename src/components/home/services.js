bbe781526238ce8508f0cd34627a443d78c5972a
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:34:15 +0300

    add meet doctors section

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -2,18 +2,36 @@ import "./home.css";
 
 export default function Services() {
   return (
-    <section className="services">
-      <h3 className="services-header">Services</h3>
-      <div className="services-container">
-        <span className="services-card"></span>
-        <span className="services-card"></span>
-        <span className="services-card"></span>
-      </div>
-      <div className="services-footer">
-        <span className="services-footer-span"></span>
-        <span className="services-footer-span"></span>
-        <span className="services-footer-span"></span>
-      </div>
-    </section>
+    <>
+      <section className="services">
+        <h3 className="services-header">Services</h3>
+        <div className="services-container">
+          <span className="services-card"></span>
+          <span className="services-card"></span>
+          <span className="services-card"></span>
+          <span className="services-card"></span>
+        </div>
+        <div className="services-footer">
+          <span className="services-footer-span"></span>
+          <span className="services-footer-span"></span>
+          <span className="services-footer-span"></span>
+        </div>
+      </section>
+
+      <section className="services">
+        <h3 className="services-header">Services</h3>
+        <div className="services-container">
+          <span className="services-card"></span>
+          <span className="services-card"></span>
+          <span className="services-card"></span>
+          <span className="services-card"></span>
+        </div>
+        <div className="services-footer">
+          <span className="services-footer-span"></span>
+          <span className="services-footer-span"></span>
+          <span className="services-footer-span"></span>
+        </div>
+      </section>
+    </>
   );
 }
