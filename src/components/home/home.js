7c52b2764bb93d0107fcb70ec7c453ba44e263b9
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 01:17:54 +0300

    connect services to home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./home.css";
 
 import MainSection from "./mainsection";
+import Services from "./services";
 
 export default function Home() {
   return (
     <main className="main">
       <MainSection />
+      <Services />
     </main>
   );
 }
--- src/components/home/services.js
+++ src/components/home/services.js
@@ -1,7 +1,7 @@
 export default function Services() {
   return (
     <section className="services">
-      <div className="services-header"></div>
+      <h3 className="services-header">Services</h3>
       <div>
         <span className="services-card"></span>
         <span className="services-card"></span>
