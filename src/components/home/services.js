10ee69723ffce61b309dfebbff34112499c5a10b
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 03:57:26 +0300

    create doctor card structure

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -21,7 +21,12 @@ export default function Services() {
       <section className="services">
         <h3 className="services-header">Meet Our Doctors</h3>
         <div className="services-container">
-          <span className="doctor-card"></span>
+          <span className="doctor-card">
+            <img className="doctor-card-image" src="" alt="doctor" />
+            <p className="doctor-crad-doc"></p>
+            <p className="doctor-crad-par"></p>
+            <button>Read More</button>
+          </span>
           <span className="doctor-card"></span>
           <span className="doctor-card"></span>
         </div>
