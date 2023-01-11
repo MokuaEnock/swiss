9ab56041a97f91960cb474aabfaafdd63f5366a5
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 18:31:22 +0300

    remove default styles

--- src/components/doctor/doctor.js
+++ src/components/doctor/doctor.js
@@ -30,13 +30,13 @@ export default function Doctor() {
 
   return (
     <main id="doctor">
-      <header id="doctor-header">
+      {/* <header id="doctor-header">
         <span id="doctor-logo">Swiss Medicare</span>
         <span className="doctor-links">Home</span>
         <span className="doctor-links">Patients</span>
         <span className="doctor-links">Profile</span>
         <span id="doctor-logout">Log Out</span>
-      </header>
+      </header> */}
 
       <section id="doctor-section">
         <div id="doctor-stats">
