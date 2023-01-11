79b4123575383edcd53abcd0ce92f8e289afcc18
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 20:08:22 +0300

    remove comented code

--- src/components/doctor/doctor.js
+++ src/components/doctor/doctor.js
@@ -1,33 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
-import { Patientsdata } from "../../data";
+import { Link } from "react-router-dom";
 import React from "react";
 import DoctorList from "./doctorlist";
 
 export default function Doctor() {
-  let navigate = useNavigate();
-
-  let patients = Patientsdata.map((e) => {
-    return (
-      <li
-        className="doctor-patient"
-        key={e.id}
-        onClick={() => {
-          navigate(`/doctor/${e.id}`);
-        }}
-      >
-        <span className="doctor-item-image"></span>
-        <span className="doctor-item-contact">
-          <h4>{e.name}</h4>
-          <p>mokuaenock0@gmail.com</p>
-        </span>
-        <span className="doctor-item-age">27 years</span>
-        <span className="doctor-item-sex">Male</span>
-        <span className="doctor-item-type">Outpatient</span>
-        <span className="doctor-item-disease">Malaria</span>
-      </li>
-    );
-  });
-
   return (
     <main id="doctor">
       <span id="doctor-header">
@@ -46,7 +21,7 @@ export default function Doctor() {
           <span className="doctor-item-bmi">B.M.I</span>
         </div>
         <DoctorList />
-        
+
         <div id="doctor-list-footer">
           <button id="doctor-back" className="doctor-nav">
             Back
