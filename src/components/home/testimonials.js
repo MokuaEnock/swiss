a004415f6812e4c54c70e4db1beeb4795946e79c
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 03:44:46 +0300

    format nd style testimonial card

--- src/components/home/testimonials.js
+++ src/components/home/testimonials.js
@@ -3,7 +3,22 @@ export default function Testimonials() {
     <section className="testimonials">
       <h3 className="services-header">Testimonials</h3>
       <div className="test-conts">
-        <span className="test-cards"></span>
+        <span className="test-cards">
+          <span className="test-cards-image">
+            <img src="" alt="profile" />
+          </span>
+          <span className="test-cards-review">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque ullamcorper et quam et tempus. Aenean ultrices varius
+              justo, faucibus pellentesque dui porttitor quis. Phasellus
+              scelerisque turpis at purus dictum.
+            </p>
+          </span>
+          <span className="test-cards-rating">
+            <img src="" alt="rating" />
+          </span>
+        </span>
         <span className="test-cards"></span>
       </div>
     </section>
