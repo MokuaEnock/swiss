c3b59305f82fb5afa81c977c4d693e06489b4686
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 03:48:34 +0300

    add content to testimonial two

--- src/components/home/testimonials.js
+++ src/components/home/testimonials.js
@@ -19,7 +19,22 @@ export default function Testimonials() {
             <img src="" alt="rating" />
           </span>
         </span>
-        <span className="test-cards"></span>
+        <span className="test-cards">
+          <span className="test-cards-image">
+            <img src="" alt="profile" />
+          </span>
+          <span className="test-cards-review">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque ullamcorper et quam et tempus. Aenean ultrices varius
+              justo, faucibus pellentesque dui porttitor quis. Phasellus
+              scelerisque turpis at purus dictum.
+            </p>
+          </span>
+          <span className="test-cards-rating">
+            <img src="" alt="rating" />
+          </span>
+        </span>
       </div>
     </section>
   );
