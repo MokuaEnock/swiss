db3763c5b21e1efa6b4b195a0530ba5ef0b2a495
Enock <enock.mokua@student.moringaschool.com>
2023-01-12 08:25:48 +0300

    style search form button

--- src/components/doctor/doctor.js
+++ src/components/doctor/doctor.js
@@ -8,18 +8,9 @@ export default function Doctor() {
         <Link to="/doctor/search">Search</Link>
         <Link to="/doctor/list">List</Link>
       </span>
-
       <section id="doctor-container">
-
-
-        {/* <DoctorList /> */}
-
-
-
         <Outlet />
       </section>
-
-      <aside id="doctor-aside"></aside>
     </main>
   );
 }
