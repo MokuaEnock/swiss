0194855e18740a0191f107d79bbbf2cfb8121866
Enock <enock.mokua@student.moringaschool.com>
2023-01-12 23:31:23 +0300

    render dynamical data

--- src/components/doctor/doctorlist.js
+++ src/components/doctor/doctorlist.js
@@ -1,33 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import { Patientsdata } from "../../data";
 import { useState, useEffect } from "react";
 
 export default function DoctorList() {
   let navigate = useNavigate();
   let [data, setData] = useState([]);
-  let [pats, setPats] = useState("");
   let [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   let fetchData = asyc ()=>{
-  //     setIsLoading(true);
-  //     try{
-  //       let response = await fetch("http://localhost:3000/api/v1/users");
-  //       let data = await response.json()
-  //       setData(data)
-  //     }catch(error){
-  //       console.log(error)
-  //     }finally{
-  //       setIsLoading(false)
-  //     }
-  //   }
-  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch("http://localhost:3000/api/v1/users");
+        const response = await fetch("http://localhost:3000/api/v1/patients");
         const data = await response.json();
         setData(data);
       } catch (error) {
@@ -38,6 +21,14 @@ export default function DoctorList() {
     };
     fetchData();
   }, []);
+  console.log(data[1]);
+
+  function Age(a, b) {
+    let date1 = new Date("2010-01-01");
+    let date2 = new Date("2020-01-01");
+    let diffInYears = date2.getFullYear() - date1.getFullYear();
+    return diffInYears;
+  }
 
   // let patients = data.map((e) => {
   //   return (
@@ -85,9 +76,9 @@ export default function DoctorList() {
                 navigate(`${e.id}`);
               }}
             >
-              <span className="doctor-item-name">{e.name}</span>
-              <span className="doctor-item-email">mok@gmail.com</span>
-              <span className="doctor-item-age">25</span>
+              <span className="doctor-item-name">{e.first_name}</span>
+              <span className="doctor-item-email">{e.email}</span>
+              <span className="doctor-item-age">{e.date_of_birth}</span>
               <span className="doctor-item-sex">Male</span>
               <span className="doctor-item-bmi">19</span>
             </li>
