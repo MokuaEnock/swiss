eac8bab0c01910781257f5a442b3d042fd5d2c88
Enock <enock.mokua@student.moringaschool.com>
2023-01-14 06:22:51 +0300

    create function for hadnleformbuttons

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+  function handleFormButtons(e) {
+    e.preventDefault();
+  }
   return (
     <main className="auth-container">
       <div className="auth-image"></div>
       <form className="authentication">
         <h3>Get started.</h3>
-        <span></span>
+        <span className="form-buttons">
+          <button onClick={handleFormButtons}>Patient</button>
+          <button onClick={handleFormButtons}>Patient</button>
+          <button onClick={handleFormButtons}>Patient</button>
+        </span>
         <input type="text" placeholder="Username" />
         <input type="text" placeholder="Email" />
         <input type="password" placeholder="Password" />
