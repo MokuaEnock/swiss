de3d4b78fd704a2bcfcad96bb030a7f26803b2dd
Enock <enock.mokua@student.moringaschool.com>
2023-01-14 06:00:14 +0300

    add input fileds to login form

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,11 +1,18 @@
 import "./auth.css";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <main className="auth-container">
       <div className="auth-image"></div>
-      <form className="authentication"></form>
+      <form className="authentication">
+        <h3>Login to Swiss Medicare</h3>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <Link to="">Forgot Password</Link>
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 }
