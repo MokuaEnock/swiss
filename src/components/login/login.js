43716414e3a83f564890ddac4d1e7f5950cb45e4
Enock <enock.mokua@student.moringaschool.com>
2023-01-14 05:52:05 +0300

    create the basic structure of login form

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,5 +2,10 @@ import "./auth.css";
 import { useState, useEffect } from "react";
 
 export default function Login() {
-  return <main className="auth-container">Login</main>;
+  return (
+    <main className="auth-container">
+      <div className="auth-image"></div>
+      <form className="authentication"></form>
+    </main>
+  );
 }
